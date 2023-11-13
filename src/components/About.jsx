57e8111a0d0f7244aa427541251214757ea46147
Hasan Wazid <Webexguruhasan95@gmail.com>
2023-11-13 22:29:43 +0600

    [COMPLETED]: about section all functionality and fixed responsive issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { fadeIn, textVariant } from "../utils/motion";
+import { services } from "../constance";
+import { SectionWrapper } from "../hoc";
 
+const ServicesCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] px-12 min-h-[250px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>OverView</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        This is Hasan Wazid from Bangladesh, a professional front-end developer
+        with over 2 years of experience. also, I'm the best WordPress developer
+        in Bangladesh with over 3 years of expertise. with my Expertise, I offer
+        professional web design services, including custom coding websites using
+        modern technologies such as HTML, CSS, Bootstrap, Tailwind, SASS,
+        JavaScript, TypeScript, React, and Next.js. Additionally, I use my
+        WordPress expertise to help clients create a strong online presence. As
+        an ecommerce expert in Bangladesh, I am proud to offer my clients the
+        best possible service.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServicesCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
