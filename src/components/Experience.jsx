59df28b6da0139e3a075885b7c235e008cdd9ad2
Hasan Wazid <Webexguruhasan95@gmail.com>
2023-11-14 21:32:03 +0600

    [COMPLETED]: experince section functionality and responsive issue

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../style";
+import { experiences } from "../constance";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experince }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    date={experince.date}
+    iconStyle={{ background: experince.iconBg }}
+    icon={
+      <div className="flex justify-center items-center w-full h-full">
+        <img
+          src={experince.icon}
+          alt={experince.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <h3 className="text-white text-[24px] font-bold">{experince.title}</h3>
+      <p
+        className="text-secondary text-[16px] font-semibold"
+        style={{ margin: 0 }}
+      >
+        {experince.company_name}
+      </p>
+    </div>
+
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experince.points.map((point, index) => (
+        <li
+          key={`experince-point-${index}`}
+          className="text-white text-[14px] pl-1 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h3 className={styles.sectionHeadText}>Work Experiences</h3>
+      </motion.div>
 
-export default Experience
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experince, index) => (
+            <ExperienceCard key={index} experince={experince} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "work");
